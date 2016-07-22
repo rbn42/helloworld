0d58e4cdfc6fa6ea599c5f715c2ddf2f9f3102eb
rbn42 <bl100@students.waikato.ac.nz>
2016-07-23 01:00:04 +1200

    c++11

---
+++ cpp11/tutorial.cpp
@@ -0,0 +1,26 @@
+#include <iostream>
+#include <math.h>
+#include <stdio.h>
+#include <stdlib.h>
+int main() //int argc, char *argv[])
+{
+    int a;
+    std::cout << "hello\n";
+    //std::cin >> a;
+    std::cout << a << std::endl;
+    int b(3);
+    int c{ 2 };
+    auto d = 1 + 2;
+    std::cout << b;
+    std::cout << c;
+    // if (argc < 2)
+    //   {
+    //   fprintf(stdout,"Usage: %s number\n",argv[0]);
+    //   return 1;
+    //   }
+    // double inputValue = atof(argv[1]);
+    // double outputValue = sqrt(inputValue);
+    // fprintf(stdout,"The square root of %g is %g\n",
+    //         inputValue, outputValue);
+    // return 0;
+}
